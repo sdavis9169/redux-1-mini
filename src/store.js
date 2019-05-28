a18558655ffc04a56accc4d23c63f11aba9d2875
Sdavis <sdavis@Stevens-MacBook-Pro.local>
2019-05-28 16:16:17 -0600

    Complete

---
+++ src/store.js
@@ -0,0 +1,23 @@
+import  { createStore } from 'redux';
+
+const initialState = { currentValue: 0 };
+
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+
+function counter (state = initialState, action)  {
+    switch(action.type) {
+        case INCREMENT:
+            return { currentValue: state.currentValue + action.amount };
+        case DECREMENT: 
+            return { currentValue: state.currentValue - action.amount };
+        default:
+        return state;
+    }
+    
+}
+
+
+
+
+export default createStore(counter);
